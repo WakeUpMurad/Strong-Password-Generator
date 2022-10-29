a3df81383c1abd6a68c173709e755b8cdc13b723
Murad <murad.losangeles@gmail.com>
2022-10-29 04:31:09 +0300

    make classes

--- script/script.js
+++ script/script.js
@@ -1,59 +1,69 @@
 
-const rotateBtn = document.getElementById("gen-btn");
-const colorIdentifier = document.getElementById("color-identifier");
-const slider = document.getElementById("myRange");
-const output = document.getElementById("password-length");
-output.value = slider.value;
-
-
-function genPassword() {
-    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let passwordLength = output.value;
-    let password = "";
-    for (let i = 0; i < passwordLength; i++) {
-        let randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber + 1);
+class Component {
+    turns = 1
+    constructor(selector) {
+        this.$el = document.querySelector(selector);
     }
-    document.getElementById("password").value = password;
+    getValue() {
+        return this.$el.value
+    }
+    setValue(val) {
+        this.$el.value = val;
+    }
+    setRotate() {
+        this.$el.style.transform = `rotate(${this.turns * 0.5}turn)`;
+        this.turns++
+    }
+    setBackgroundColor(backgroundColor) {
+        this.$el.style.backgroundColor = backgroundColor;
+    }
+    setWidth(width) {
+        this.$el.style.width = width;
+    }
+}
 
-    if (slider.value <= 8 ) {
-        colorIdentifier.style.backgroundColor = 'red';
-        colorIdentifier.style.width = '25%';
-        colorIdentifier.style.borderTopRightRadius = '0.5rem';
-        colorIdentifier.style.transition = '1s';
-    } else if (slider.value > 8 &&  slider.value <= 14 ){
-        colorIdentifier.style.backgroundColor = 'orange';
-        colorIdentifier.style.width = '50%';
-    } else if (slider.value > 14 ) {
-        colorIdentifier.style.backgroundColor = 'green';
-        colorIdentifier.style.width = '100%';
+const slider = new Component("#sliderValue");
+const select = new Component("#selectValue");
+const password = new Component("#password");
+const colorIdentifier = new Component("#color-identifier");
+const rotateBtn = new Component("#gen-btn");
+
+
+class PasswordGenerator {
+
+    chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    genPassword(passwordLength = select.getValue()) {
+        select.setValue(passwordLength);
+        slider.setValue(passwordLength);
+        let newPassword = '';
+        for (let i = 0; i < passwordLength; i++) {
+            let randomNumber = Math.floor(Math.random() * this.chars.length);
+            newPassword += this.chars.substring(randomNumber, randomNumber + 1);
+        }
+        password.setValue(newPassword)
+
+        if (slider.getValue() <= 8 ) {
+            colorIdentifier.setBackgroundColor('red')
+            colorIdentifier.setWidth('25%')
+        } else if (slider.getValue() > 8 &&  slider.getValue() <= 14 ){
+            colorIdentifier.setBackgroundColor('orange')
+            colorIdentifier.setWidth('50%')
+        } else if (slider.getValue() > 14 ) {
+            colorIdentifier.setBackgroundColor('green')
+            colorIdentifier.setWidth('100%')
+        }
     }
 
+    copyPassword() {
+        password.$el.select();
+        document.execCommand("copy");
+    }
 
 }
 
-function genPasswordSlider() {
-    output.value = slider.value;
-    genPassword()
-}
-
-function genPasswordSelect() {
-    slider.value = output.value;
-    genPassword()
-}
-
-let rotation = 0;
-const angle = 180;
-
-function genPasswordRefresh() {
-    rotation += angle;
-    rotateBtn.style.transform = `rotate(${rotation}deg)`;
-    rotateBtn.style.transition = '1s';
-    genPassword();
-}
-
-function copyPassword() {
-    let copyText = document.getElementById("password");
-    copyText.select();
-    document.execCommand("copy");
+const genNewPassword = new PasswordGenerator();
+const doRotateBtn = () => {
+    genNewPassword.genPassword();
+    rotateBtn.setRotate();
 }
